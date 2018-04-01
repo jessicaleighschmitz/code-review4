69689e0048500e8056924dad1193450bc265d5fa
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-03-31 21:34:48 -0700

    add working js

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ function Pizza(size, toppings, price) {
   this.toppings = toppings;
   this.price = price;
 }
-Pizza.prototype.totalPrice = function () {
-  var total = 0;
 
+Pizza.prototype.sizePrice = function(size, toppingsArray) {
+  var total = 0;
   if (size === "Small") {
     total += 6;
   } else if (size === "Medium") {
@@ -13,7 +13,14 @@ Pizza.prototype.totalPrice = function () {
   } else if (size === "Large") {
     total += 10;
   }
-  return total;
+  for (var i = 0; i < toppingsArray.length; i++) {
+    total += 1;
+    console.log(total);
+    this.price = total;
+}
+}
+Pizza.prototype.cost = function (size, toppings, price) {
+  return "$" + this.price + " " + this.size + " with " + this.toppings;
 }
 
 
@@ -21,20 +28,25 @@ $(document).ready(function() {
   $('#form').submit(function(event) {
     event.preventDefault();
 
-    var size = $('#pizza-size').val();
+    var sizeNew = $('#pizza-size').val();
     var toppings = $('input:checkbox[name=toppings]:checked');
     var toppingsArray = [];
     for(var i=0; i< toppings.length; i++){
-
       toppingsArray.push($(toppings[i]).val());
     }
 
-    var newPizza = new Pizza(size, toppingsArray);
 
 
-
-
+    var newPizza = new Pizza(sizeNew, toppingsArray, price);
+    var price = newPizza.sizePrice(sizeNew, toppingsArray);
+    var output = newPizza.cost(sizeNew, toppingsArray, price);
     $('#pizza-order').show();
-    $('.order').text(newPizza.size + newPizza.toppings);
+    $('.order').text(output);
   });
+  $('#final-order').click(function(event) {
+    event.preventDefault();
+
+    $('#container').hide();
+    $('#thank-you').show();
+  })
 });
